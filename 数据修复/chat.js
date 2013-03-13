fc5033c9ca3647b41d9ad2894cee9b890c35909d
jun.lu <idche@qq.com>
2013-03-13 10:26:56 +0800

    数据修复代码

--- 数据修复/chat.js
+++ 数据修复/chat.js
@@ -1,4 +1,13 @@
 
+/**
+	修复chat表的 
+	update	Uid->uid   
+			Uname->uname
+	add		uavatar
+	del 	index
+
+
+*/
 var users = db.user.find();
 var chats = db.chat.find();
 while(chats.hasNext()){
@@ -11,12 +20,10 @@ while(chats.hasNext()){
 	}
 
 	if(item.Uid){
-		//print(item.Uid);
 		item.uid = item.Uid;
 		item.uname = item.Uname;
-		//print(db.user.findOne({_id:item.Uid}).hexMail);
 		var hexmail = db.user.findOne({_id:item.Uid}).hexMail;
-		item.uavatar = "http://www.gravatar.com/avatar/{hash}.jpg?s=48&d=mm".replace('{hash}', hexmail);
+		item.uavatar = "http://www.gravatar.com/avatar/{hash}.jpg?s=48&d=monsterid".replace('{hash}', hexmail);
 		delete item.Uid;
 		delete item.Uname;
 		delete item.index;
