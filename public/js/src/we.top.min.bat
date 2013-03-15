79a26a4f4d71c216cfc479725eea626fa35e38df
Huang.CK <549596429@qq.com>
2013-03-15 17:49:42 +0800

    删除一些没用的文件以及压缩

---
+++ public/js/src/we.top.min.bat
@@ -0,0 +1,13 @@
+@echo off
+set inputlist=%inputlist% --js we.top\we.api.top.js
+set inputlist=%inputlist% --js we.top\we.page.top.js
+
+
+
+set outputfilename= --js_output_file ..\we.top.min.js
+echo %inputlist%
+echo %outputfilename%
+
+java -jar compiler.jar %inputlist% %outputfilename%
+echo 文件已输出到 %outputfilename%
+@pause>nul
