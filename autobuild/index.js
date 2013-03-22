17d272f15e84c46c262101580895521829a22f15
jun.lu <idche@qq.com>
2013-03-22 10:33:06 +0800

    hello

--- autobuild/index.js
+++ autobuild/index.js
@@ -21,9 +21,12 @@ var files = analysis.readdirFile( paths );
 //根据日志文件筛选出更新过的资源文件
 var updateFiles = analysis.fileRecordExtract(files, logs);
 
-//把更新过的文件创建副本
-var newVersionFiles = analysis.createNewVersionFile( updateFiles );
-//console.log( newVersionFiles );
+for(var i=0; i<updateFiles.length; i++){
+    console.log("更新:", updateFiles[i].getFilePath());
+}
+
+// 创建新的文件版本
+var newVersionFiles = analysis.createNewVersionFiles( updateFiles );
 
 //所有需要替换的文本文件
 var viewFiles = analysis.readViewFiles( viewPaths );
@@ -31,7 +34,13 @@ var viewFiles = analysis.readViewFiles( viewPaths );
 //替换所有资源路径
 analysis.replaceResourcesPath( viewFiles, newVersionFiles );
 
-analysis.saveNewLogs( logFilePath, logs, newVersionFiles );
+//创建副本
+analysis.createFiles( newVersionFiles );
+
+newVersionFiles = analysis.createNewVersionFile( updateFiles );
+
 
 //删除旧版本文件
 analysis.deleteVersionFile( updateFiles );
+
+console.log("-------------ok------------");
