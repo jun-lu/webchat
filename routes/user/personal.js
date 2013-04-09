14e5edb68dccbdd6070d85a3ba3a091520c9a0b9
jun.lu <idche@qq.com>
2013-04-09 16:27:27 +0800

    个人也bug

--- routes/user/personal.js
+++ routes/user/personal.js
@@ -42,14 +42,14 @@ module.exports = {
 					//console.log( accessUser );
 					LogModel.getLog( accessUser._id, 1000, function( status ){
 
+						//进入过
+						var intos = [];
+						//创建过
+						var creates = [];
+
 						if(status.code == "0"){
 
 							var logs = status.result;
-							//进入过
-							var intos = [];
-							//创建过
-							var creates = [];
-
 							for(var i=0; i< logs.length; i++){
 
 								logs[i].info.time = logs[i].time;
@@ -64,20 +64,15 @@ module.exports = {
 								}
 							}
 
-							res.render("user/personal", status.toJSON({
-								user:user ? user.getInfo() : user,
-								accessUser:accessUser.getPublicInfo( 180 ),
-								creates:creates,
-								intos:tools.unique(intos, function(item){  return item.id; }),
-								tool:tools
-							}));
-
-						}else{
-
-							res.write( status.toJSON() );
-							res.end();
-
 						}
+
+						res.render("user/personal", status.toJSON({
+							user:user ? user.getInfo() : user,
+							accessUser:accessUser.getPublicInfo( 180 ),
+							creates:creates,
+							intos:tools.unique(intos, function(item){  return item.id; }),
+							tool:tools
+						}));
 					} )
 
 
