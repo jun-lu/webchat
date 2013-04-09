95bf4f5e44da519e84ca6b404065149a5afccf9a
jun.lu <idche@qq.com>
2013-04-09 16:51:11 +0800

    清楚旧的cookie

--- routes/sys/weibo_login.js
+++ routes/sys/weibo_login.js
@@ -95,7 +95,9 @@ module.exports = {
 						}else{
 
 							var user = status.result;
-							res.setHeader("Set-Cookie", ["sid="+user.toCookie()+";path=/;domain="+config.domain+";expires="+new Date("2030") ]);
+							res.setHeader("Set-Cookie", ["sid="+user.toCookie()+";path=/;domain="+config.domain+";expires="+new Date("2030"),
+								"sid=0|0|0;path=/;domain=vchat.co;expires="+new Date("2000")
+								]);
 							res.render("sys/sina_login", {state:state, user:user.getInfo()} );
 							res.end();
 						}
