b227f1a240f45cfc5669a4219e9b125b2aa4c05b
jun.lu <idche@qq.com>
2013-04-18 13:35:01 +0800

    匿名用户创建房间bug

--- routes/sys/create.js
+++ routes/sys/create.js
@@ -36,7 +36,7 @@ module.exports = {
 			UserModel.createAnonymousUser( function( status ){
 
 				if(status.code == 0){
-					user = status.result.getInfo();
+					user = status.result;
 					res.setHeader("Set-Cookie", ["sid="+user.toCookie()+";path=/;expires="+new Date("2030") ]);
 					masterid = user._id;
 					create();
