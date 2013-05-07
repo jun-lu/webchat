de975e8f07c124c9f9aa06b3e5199bd92ab9d808
Huang.CK <549596429@qq.com>
2013-05-07 23:07:48 +0800

    添加支持markdown语法

---
+++ public/js/src/we.min/we.markdown.js
@@ -0,0 +1,23 @@
+WE.extend( WE.markdown,{
+
+	defaultPrefix : '````',
+
+	default : function( text ){
+
+		var _this = this;
+
+		if( text.length > 0 ){
+
+			var prefixLen = text.split( _this.defaultPrefix ).length - 1;
+
+			if( prefixsLen != 0 && prefixsLen % 2 == 0 ){
+
+					
+			}
+
+		}else{
+
+			return false;
+		}
+	}
+})
