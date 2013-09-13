5d10d4494c8cbcdb6c8c155416674df82dfebbfe
Jun <idche@qq.com>
2013-09-13 15:24:28 +0800

    upload.js

--- upload.js
+++ upload.js
@@ -3,5 +3,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
   res.write('Hello\n vchat.co 正在升级...大约还需要3小时', 'utf-8');
   res.end();
-}).listen(80, '127.0.0.1');
+}).listen(80);
 console.log('Server running at http://127.0.0.1:80/');
