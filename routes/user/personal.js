07b35f6366e4fbe46c4fb7d26289e3b067d5f1a5
Jun <idche@qq.com>
2013-09-28 14:34:20 +0800

    personal.js

--- routes/user/personal.js
+++ routes/user/personal.js
@@ -9,11 +9,9 @@ var tools = require("../../lib/tools");
 var UserModel = require("../../lib/UserModel");
 var RoomModel = require('../../lib/RoomModel');
 var ChatModel = require('../../lib/ChatModel');
+var Promise = require('../../lib/Promise');
 var LogModel = require('../../lib/LogModel');
 var WebStatus = require('../../lib/WebStatus');
-var socketServer = require('../../lib/socketServer');
-//var maxIndex = {};
-var roomLimit = require("../sys/room_limit");
 
 
 
@@ -25,6 +23,14 @@ module.exports = {
 			var id = req.params.key;
 			var status = new WebStatus();
 
+			var output = {
+				user:user ? user.getInfo() : user,
+				intos:[], //进入过的话题
+				creates:[],//我创建过的话题
+				accessUser:null, //被访问者信息
+				tool:tools
+			}
+
 			//512081d4ff08ee5e51000001
 			if(id.length != 24){
 
@@ -34,59 +40,62 @@ module.exports = {
 				return false;	
 			};
 
-			//console.log(id);
+			var promise = new Promise();
+			//查询被访问者信息
+			promise.add(function() {
 
-			UserModel.find_id(id, function( status ){
+				UserModel.find_id(id, function( status ){
 
-				if(status.code == "0"){
+					if(status.code == "0"){
+						output.accessUser = status.result;
+						promise.ok( status.result );	
+					}else{
+						status.addMsg("没有找到用户");
+						res.status(404).render("404", status.toJSON() );
+					}
 
-					var accessUser = status.result;
-					//console.log( accessUser );
-					LogModel.getLog( accessUser._id, 1000, function( status ){
+				});
+			});
 
-						//进入过
-						var intos = [];
-						//创建过
-						var creates = [];
+			promise.then(function( user ) {
+				RoomModel.querys({masterId:String(user._id)}, function( status ) {
+					if( status.code == 0 ){
 
-						if(status.code == "0"){
+						output.creates = status.result;
 
-							var logs = status.result;
-							for(var i=0; i< logs.length; i++){
+					}
+					promise.ok();
+				});
+			});
 
-								logs[i].info.time = logs[i].time;
-								if(logs[i].location == "into_room"){
+			promise.add(function ( user ) {
+				LogModel.getLog( user._id, 1000, function( status ) {
+					//进入过
+					var intos = output.intos;
+					if(status.code == "0"){
 
-									intos.push( logs[i].info );
-								}
-
-								if(logs[i].location == "create_room"){
-
-									creates.push( logs[i].info );
-								}
+						var logs = status.result;
+						for(var i=0; i< logs.length; i++){
+							logs[i].info.time = logs[i].time;
+							if(logs[i].location == "into_room"){
+								intos.push( logs[i].info );
 							}
-
 						}
 
-						res.render("user/personal", status.toJSON({
-							user:user ? user.getInfo() : user,
-							accessUser:accessUser.getPublicInfo( 180 ),
-							creates:creates,
-							intos:tools.unique(intos, function(item){  return item.id; }),
-							tool:tools
-						}));
-					} )
+					};
 
-
-				}else{
-					//console.log("-----------------","no find");
-					status.addMsg("没有找到用户");
-
-					res.status(404).render("404", status.toJSON() );
-				}
-
+					promise.ok();	
+				});
 			});
+
+			promise.then(function() {
+				res.render("user/personal", output);
+			});
+
+
+			promise.start();
 		},
+
 		
 		post:function( req, res ){
 
