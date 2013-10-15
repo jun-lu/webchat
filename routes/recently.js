891341c1dfb4815933258d62230b2ebdd1919acc
Jun <idche@qq.com>
2013-10-15 12:13:03 +0800

    add

--- routes/recently.js
+++ routes/recently.js
@@ -13,29 +13,27 @@ module.exports = {
 	get:function(req, res){
 
 		var user = req.session.user;
-		var time = req.query.time || parseInt(Date.now()/1000);
-		var a = req.query.a || "next";
-		var query = {};
+		var time = parseInt(req.query.time) || parseInt(Date.now()/1000);
+		var page = parseInt(req.query.page) || 1;
+		var pageSize = 10;
+
 		var output = {
 			user:user ? user.getInfo() : null,
 			rooms:[],
 			tool:tools,
-			time:0
+			prevPage: page - 1 > 0 ? page -1 : -1,
+			nextPage: page + 1
 		};
 
-		if(a == "next"){
-			query = {"$lt":time};
-		}else{
-			query = {"$gt":time};
-		}
 
-		RoomModel.findLimitSort({time:query}, 10, {time:-1},function( status ){
+		RoomModel.findLimitSkipSort({}, 10, (page-1) * pageSize, {time:-1},function( status ){
 			RoomModel.serialization( status, function( status ){
-				//console.log("status", status);
+				console.log( "status", status );
 				output.rooms = status.result;
-				if( status.code == 0 ){
-					output.time = output.rooms[output.rooms.length-1].time;
-				}			
+				if( status.result.length < pageSize ){
+					output.nextPage = -1;
+				}
+
 				res.render("recently", output);
 			} );
 			
